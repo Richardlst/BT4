137272f7448269628e2c8b3cf7a8e98916cef3ea
Richardlst <lesytoan2003@gmail.com>
2023-03-02 00:54:15 +0700

    up lan 2

--- bai 13 (BT4).cpp
+++ bai 13 (BT4).cpp
@@ -1,24 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool ktr(int a[], int n){
+bool ktr(int *a, int n){
 
 for(int i=0; i<n; i++){
         int t1=0, t2=0;
     for(int j=0; j<i; j++) t1+=a[j];
     for(int q=i+1; q<n; q++) t2+=a[q];
     if(t1==t2) return true;
-    else return false;
-}
+    else continue;}
+    return false;
+
 }
 int main(){
-    int t, a[10000];
+    int t;
+    int *a=new int[10005];
     cin>>t;
     for(int i=0; i<t; i++){
         int n;
         cin>>n;
         for(int j=0; j<n; j++) cin>>a[j];
-        if(ktr(a, n)) cout<<"yes";
-        else cout<<"no";
+        if(ktr(a, n)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
+    delete []a;
 return 0;
 }
